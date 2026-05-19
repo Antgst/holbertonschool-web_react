e6d149fe1d06d708ba29b8fd603122121f4b6bad
Antoine Gousset <ant.gousset@gmail.com>
2026-05-19 17:10:22 +0200

    add: resolve T2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,19 +1,33 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 const teacher3: Teacher = {
-    firstName: "Léa",
-    lastName: "Toutous",
-    fullTimeEmployee: true,
-    yearsOfExperience: 0,
-    location: "Cesson-sevigne",
-    contract: true,
+  firstName: 'Léa',
+  lastName: 'Toutous',
+  fullTimeEmployee: true,
+  yearsOfExperience: 0,
+  location: 'Cesson-sevigne',
+  contract: true,
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'Louiza',
+  lastName: 'Houaouti',
+  location: 'Rennes',
+  fullTimeEmployee: true,
+  numberOfReports: 9,
+};
+
+console.log(director1);
